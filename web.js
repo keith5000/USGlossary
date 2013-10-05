4b43cd82a0a6fc353dfc9dba9036ca6414b08e1b
keith5000 <keith5000@gmail.com>
2013-10-05 15:55:28 -0400

    now capturing raw urls from payload

--- web.js
+++ web.js
@@ -4,14 +4,30 @@ var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
 
+/* not needed - left for testing purposes */
 app.get('/', function(request, response) {
   response.send('Hello World!');
-  console.log("request = " + util.inspect(request.params));
 });
 
 app.post('/', function(request, response) {
-  response.send('Hello World!');
-  console.log("request = " + util.inspect(request.body));
+	if (!request || !request.body || !request.body.commits) return;
+	
+	console.log("request = " + util.inspect(request.body));
+	
+	var masterBranchBaseUrl = "https://raw.github.com/keith5000/USGlossary/";
+	
+	for (var commitCount=0; commitCount < request.body.commits.length; commitCount++) {
+		for (var addedCount=0; addedCount < request.body.commits.added; addedCount++) {
+			var rawFileUrl = masterBranchBaseUrl + request.body.commits.added[addedCount];
+			console.log("Added url: " + rawFileUrl);
+		}
+		
+		for (var modifiedCount=0; modifiedCount < request.body.commits.modified; modifiedCount++) {
+			var rawFileUrl = masterBranchBaseUrl + request.body.commits.modified[modifiedCount];
+			console.log("Modified url: " + rawFileUrl);
+		}
+	}
+  
 });
 
 var port = process.env.PORT || 5000;
