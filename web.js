5567328cc3369cd1bdaaabd8fc6471c48c4f753e
keith5000 <keith5000@gmail.com>
2013-10-05 14:20:15 -0400

    adding githubhook

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var githubhook = require('githubhook');
+var github = githubhook({/* options */});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+github.listen();
+
+github.on('push:https://github.com/keith5000/USGlossary/tree/CommitTest', function (ref, data) {
+	response.send("push");
 });
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+github.on('commit:https://github.com/keith5000/USGlossary/tree/CommitTest', function (ref, data) {
+	response.send("commit");
 });
