587815646123801923a0856f82c2fd16debdd10e
keith5000 <keith5000@gmail.com>
2013-10-05 15:04:08 -0400

    removed githubhook, using express listener

--- web.js
+++ web.js
@@ -1,5 +1,21 @@
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+  console.log("request = " + request);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+
+/*
 var githubhook = require('githubhook');
-var github = githubhook({/* options */});
+var github = githubhook({ port: process.env.PORT || 3240 });
 
 github.listen();
 
@@ -10,3 +26,4 @@ github.on('push:https://github.com/keith5000/USGlossary/tree/CommitTest', functi
 github.on('commit:https://github.com/keith5000/USGlossary/tree/CommitTest', function (ref, data) {
 	response.send("commit");
 });
+*/
